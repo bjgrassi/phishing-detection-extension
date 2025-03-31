6a7e40c6839420408d9a655fd68c7e31605a8108
Bruna <brukgrassi@gmail.com>
2025-03-30 20:29:52 -0400

    making the API call

--- background.ts
+++ background.ts
@@ -1,34 +1,35 @@
+const BASE_URL = 'https://6a71-99-235-211-164.ngrok-free.app/'
+
 // Listen for messages from content script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "postUrl") {
-        console.warn("Background", request.url)
-      postUrl(request.url).then(isPhishing => {
-        sendResponse({ isPhishing })
-      })
-      return true // Required to use sendResponse asynchronously
-    }
+  if (request.action === "postUrl") {
+      console.warn("Background", request.url)
+    postUrl(request.url).then(isPhishing => {
+      sendResponse({ isPhishing })
+    })
+    return true // Required to use sendResponse asynchronously
+  }
 })
 
 const postUrl = async (url: string): Promise<boolean> => {
-    try {
-      // REPLACE MOCK DATA
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          title: 'foo',
-          body: 'bar',
-          userId: 1,
-        })
-      });
-      
-      const data = await response.json()
-      // REPLACE MOCK DATA
-      return "https://clients.cowangroup.ca/clients/content/login/loginPwd.cfm" == url;
-    } catch (error) {
-      console.error("Phishing check failed:", error)
-      return false
-    }
+  let isPhishing = false
+  try {
+    const response = await fetch(BASE_URL + "model/1", {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({ url: url })
+    });
+    const data = await response.json()
+    console.warn("response", response)
+    console.warn("json", data)
+    isPhishing = data['1'] === 1
+    return isPhishing
+  } catch (error) {
+    console.error("Phishing check failed:", error)
+    return isPhishing
   }
+}
